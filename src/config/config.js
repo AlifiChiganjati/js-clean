25ed10da630d61a1c0e023802e71cc659e8b0113
Alifi Chiganjati <alifichiganjati@gmail.com>
2024-11-24 05:38:40 +0700

    feat: add 3sec sleep before connect db

--- src/config/config.js
+++ src/config/config.js
@@ -2,15 +2,9 @@ import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const { Pool } = pg;
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: String(process.env.DB_PASS),
-  database: process.env.DB_NAME,
-});
+await sleep(3000);
 if (
   process.env.DB_HOST === "" ||
   process.env.DB_PORT === "" ||
@@ -20,5 +14,14 @@ if (
 ) {
   throw new Error("envirovment required");
 }
+
+const { Pool } = pg;
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: String(process.env.DB_PASS),
+  database: process.env.DB_NAME,
+});
 
 export default pool;
