63580d12d12b29bc75994da47195a4b661a14f22
Alifi Chiganjati <alifichiganjati@gmail.com>
2024-11-23 23:36:49 +0700

    feat: add validation for config is env not require

--- src/config/config.js
+++ src/config/config.js
@@ -11,5 +11,14 @@ const pool = new Pool({
   password: String(process.env.DB_PASS),
   database: process.env.DB_NAME,
 });
+if (
+  process.env.DB_HOST === "" ||
+  process.env.DB_PORT === "" ||
+  process.env.DB_USER === "" ||
+  process.env.DB_PASS === "" ||
+  process.env.DB_NAME === ""
+) {
+  throw new Error("envirovment required");
+}
 
 export default pool;
